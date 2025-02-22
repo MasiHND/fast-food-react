8588f819ccd2b4a27f68cbb134cc07ebd4e22477
MasiHND <pouya.hendy@gmail.com>
2025-02-22 12:13:38 +0330

    implemented fetching order

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
+import SerachOrder from "../features/order/SerachOrder"
 
 function Header() {
     return (
         <header>
            <Link to="/">Home</Link> 
+           <SerachOrder />
         </header>
     )
 }
